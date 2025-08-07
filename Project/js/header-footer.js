54ed34f26d91573b4fe023a96462470ccd636371
Tyyy09 <164542500+Tyyy09@users.noreply.github.com>
2025-08-06 20:27:26 -0400

    Add files via upload

---
+++ Project/js/header-footer.js
@@ -0,0 +1,22 @@
+
+document.getElementById("site-header").innerHTML = `
+  <nav class="navbar">
+    <div class="navbar-container">
+      <a href="index.html" class="logo">
+        <img src="images/logo.png" alt="Casio Logo" height="40">
+      </a>
+      <ul class="nav-menu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="shop.html">Shop</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+`;
+
+document.getElementById("site-footer").innerHTML = `
+  <footer class="footer">
+    <p>&copy; 2025 Casio Retro | All Rights Reserved</p>
+  </footer>
+`;
